78d1e2ad78ff3c11aacbbfd1bf6e57c435102908
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-05-28 01:00:33 +0200

    Fix blocking

--- .history/pages/[meetupId]/index_20220528003328.js
+++ .history/pages/[meetupId]/index_20220528003328.js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
 
   client.close();
   return {
-    fallback: false, // true lub 'blocking'
+    fallback: false,
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
